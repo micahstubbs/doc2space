e7c58185a76f4d669ff24c73b66ae8e66d27f122
Micah Stubbs <micah.stubbs@gmail.com>
2019-10-15 01:25:13 -0700

    notebook save nb.ipynb 20191015012513

--- notebooks/embed-text-doc2vec/nb.py
+++ notebooks/embed-text-doc2vec/nb.py
@@ -215,6 +215,23 @@ print(imported_vectors[0:2])
 
 
 # project from 20D to 2D with t-SNE
+from __future__ import print_function
+import time
+import numpy as np
+import pandas as pd
+from sklearn.datasets import fetch_openml
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+get_ipython().run_line_magic('matplotlib', 'inline')
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import seaborn as sns
+
+
+# In[ ]:
+
+
+
 
 
 # In[31]:
