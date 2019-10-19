2dd14ce1601122a55b69e477621582d802c5817f
micahstubbs <micah.stubbs@gmail.com>
2019-10-19 01:33:07 -0700

    add script to generate an image from the first page of a pdf

---
+++ scripts/pdf-to-jpg-first-page.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+infile=${1}
+
+# gs -q -o $(basename "${infile}")_p%04d.jpeg -sDEVICE=jpeg -dFirstPage=1 -dLastPage=1 "${infile}"
+
+# To get thumbnail JPEGs with a width 200 pixel use the following command:
+# gs -q -o name_200px_p%04d.jpg -sDEVICE=jpeg -dPDFFitPage -g200x400 -dFirstPage=1 -dLastPage=1 "${infile}"
+
+# To get higher quality JPEGs (but also bigger-in-size ones) with a 
+# resolution of 300 dpi use the following command:
+gs -q -o name_300dpi_p%04d.jpg -sDEVICE=jpeg -dJPEGQ=100 -r300 -dFirstPage=1 -dLastPage=1 "${infile}"
+
+echo "Done"
+
+# source
+# https://stackoverflow.com/questions/11828528/display-first-page-of-pdf-as-image
