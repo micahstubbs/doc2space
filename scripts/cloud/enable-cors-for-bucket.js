80fa41bf7012a8fb4fabea9eb4c287f22254125d
micahstubbs <micah.stubbs@gmail.com>
2019-11-17 22:28:17 -0800

    add deploy script and fix path in cors script

--- scripts/cloud/enable-cors-for-bucket.js
+++ scripts/cloud/enable-cors-for-bucket.js
@@ -1,7 +1,8 @@
+const path = require('path')
 const runShellCommand = require('../util/run-shell-command.js')
 
 const bucketName = process.env.DOC2SPACE_BUCKET_NAME
-const configFile = '../config/cors-json-file.json'
+const configFile = path.join(__dirname, '../../config/cors-json-file.json')
 const command = `gsutil cors set ${configFile} gs://${bucketName}`
 
 runShellCommand(command)
