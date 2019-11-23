f8384d7f2bebca9336613308ddaecabfcd367211
micahstubbs <micah.stubbs@gmail.com>
2019-11-23 00:34:15 -0800

    remove commented code from detect-controllers

--- frontends/webvr/detect-controllers.js
+++ frontends/webvr/detect-controllers.js
@@ -16,28 +16,6 @@ function detectControllers() {
     navigator.getVRDisplays().then(function(displays) {
       console.log(displays.length + ' displays')
       for (var i = 0; i < displays.length; i++) {
-        var cap = displays[i].capabilities
-        // cap is a VRDisplayCapabilities object
-        // var listItem = document.createElement('li')
-        // listItem.innerHTML =
-        //   '<strong>Display ' +
-        //   (i + 1) +
-        //   '</strong>' +
-        //   '<br>VR Display ID: ' +
-        //   displays[i].displayId +
-        //   '<br>VR Display Name: ' +
-        //   displays[i].displayName +
-        //   '<br>Display can present content: ' +
-        //   cap.canPresent +
-        //   "<br>Display is separate from the computer's main display: " +
-        //   cap.hasExternalDisplay +
-        //   '<br>Display can return position info: ' +
-        //   cap.hasPosition +
-        //   '<br>Display can return orientation info: ' +
-        //   cap.hasOrientation +
-        //   '<br>Display max layers: ' +
-        //   cap.maxLayers
-        // console.log(listItem)
         console.log('displays[i].displayName', displays[i].displayName)
       }
 
@@ -52,25 +30,6 @@ function detectControllers() {
     for (var i = 0; i < gamepads.length; ++i) {
       var gp = gamepads[i]
       if (gp) {
-        // var listItem = document.createElement('li')
-        // listItem.classList = 'gamepad'
-        // listItem.innerHTML =
-        //   '<strong>Gamepad ' +
-        //   gp.index +
-        //   '</strong> (' +
-        //   gp.id +
-        //   ')' +
-        //   '<br>Associated with VR Display ID: ' +
-        //   gp.displayId +
-        //   '<br>Gamepad associated with which hand: ' +
-        //   gp.hand +
-        //   '<br>Available haptic actuators: ' +
-        //   gp.hapticActuators.length +
-        //   '<br>Gamepad can return position info: ' +
-        //   gp.pose.hasPosition +
-        //   '<br>Gamepad can return orientation info: ' +
-        //   gp.pose.hasOrientation
-        // console.log(listItem)
         console.log('gp.id', gp.id)
         updateControllerHelpText(gp.id)
       }
